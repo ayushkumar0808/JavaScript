a21e4c2792475060226109de639e044de52c3d1f
Ayush Kumar <kayush3647@gmail.com>
2026-09-04 19:23:36 +0530

    update

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 // console.log("hello world");
 
+//Concatinate
+
 // let name = prompt("Enter Your Name ");
 // let name1 = prompt("Enter your last name");
 // let fulName = name + " " + name1;
 // console.log(fulName);
+
+//Type Checking
+
+// let a = 1;
+// let name = "Ayush";
+// let isTrue = true;
+// console.log(typeof a);
+// console.log(typeof name);
+// console.log(typeof isTrue);
