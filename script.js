da7ea55ba6140a568f4b05d9ca3eef28839caab3
Ayush Kumar <kayush3647@gmail.com>
2026-09-04 19:19:21 +0530

    update

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("hello world");
+// console.log("hello world");
+
+// let name = prompt("Enter Your Name ");
+// let name1 = prompt("Enter your last name");
+// let fulName = name + " " + name1;
+// console.log(fulName);
