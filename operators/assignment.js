fd590d5a7fc26dd755db42dca2f5df0c89b2c9bf
Ayush Kumar <kayush3647@gmail.com>
2026-09-04 22:54:52 +0530

    add

---
+++ operators/assignment.js
@@ -0,0 +1,20 @@
+let a = 5;
+console.log(a);
+
+a += 5;
+console.log(a);
+
+a -= 3;
+console.log(a);
+
+a *= 6;
+console.log(a);
+
+a /= 2;
+console.log(a);
+
+a %= 6;
+console.log(a);
+
+a **= 2;
+console.log(a);
