646c55e8e3735cd21f2af24f6dc73184501e9324
Ayush Kumar <kayush3647@gmail.com>
2026-09-05 16:55:19 +0530

    add

--- operators/ternary.js
+++ operators/ternary.js
@@ -1,3 +1,12 @@
-let age = 18;
-let message = age >= 18 ? "adult" : "minnor";
-console.log(message);
+// let age = 18;
+// let message = age >= 18 ? "adult" : "minnor";
+// console.log(message);
+
+//nested ternary oprator
+
+let a = 5;
+let b = 1;
+let c = 2;
+
+const result = a > b ? (a > c ? a : c) : b > c ? b : c;
+console.log(result);
