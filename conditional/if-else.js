82411183b294ec01ecd43b490c973da538c5443d
Ayush Kumar <kayush3647@gmail.com>
2026-09-06 00:26:26 +0530

    add

---
+++ conditional/if-else.js
@@ -0,0 +1,11 @@
+// let age = 20;
+// if (age >= 18) {
+//   console.log("you are allowed");
+// }
+
+let age = 10;
+if (age >= 18) {
+  console.log("you are eligible to vote");
+} else {
+  console.log("you are not eligible to vote");
+}
