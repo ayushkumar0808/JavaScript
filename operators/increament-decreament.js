1ccf2962ee0de387de529b003333b85a5e1c3c92
Ayush Kumar <kayush3647@gmail.com>
2026-09-05 16:04:52 +0530

    add

---
+++ operators/increament-decreament.js
@@ -0,0 +1,18 @@
+let a = 5;
+console.log(a++);
+console.log(a);
+
+let b = 7;
+console.log(++b);
+console.log(b);
+
+let c = 5;
+console.log(c--);
+console.log(c);
+
+let d = 7;
+console.log(--d);
+console.log(d);
+
+let e = 10;
+console.log(e++ + ++e - 5);
