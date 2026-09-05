6452e4b43e5db74cfec5d610d2eaddf9b8cbefdd
Ayush Kumar <kayush3647@gmail.com>
2026-09-06 00:39:41 +0530

    add

---
+++ conditional/nested-if.js
@@ -0,0 +1,12 @@
+let age = 10;
+let hadId = false;
+
+if (age >= 18) {
+  if (hadId) {
+    console.log("You are allowed");
+  } else {
+    console.log("Take Id first");
+  }
+} else {
+  console.log("You are not allowed");
+}
