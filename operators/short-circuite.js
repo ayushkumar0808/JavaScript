73b4e5ff77cb97b14d6f029d2e9a41519aac0bab
Ayush Kumar <kayush3647@gmail.com>
2026-09-05 18:35:43 +0530

    add

---
+++ operators/short-circuite.js
@@ -0,0 +1,5 @@
+console.log(false || "" || undefined || null || "Ayush" || true || 0);
+console.log(true && "Ayush" && "" && 2);
+console.log(null ?? ""); //nullish coalescing
+console.log(undefined ?? "Ayush");
+console.log(false ?? "");
