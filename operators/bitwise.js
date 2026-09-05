0293c9b7f1c9b3ea33c40edb8edb1dddc5feea88
Ayush Kumar <kayush3647@gmail.com>
2026-09-05 18:01:33 +0530

    add

---
+++ operators/bitwise.js
@@ -0,0 +1,8 @@
+console.log(8 & 5);
+console.log(8 | 5);
+console.log(~2); // -(x+1)
+console.log(~-5);
+console.log(-2 & 5);
+console.log(-5 | 2);
+console.log(-5 & -2);
+console.log(-5 | -2); // 5 2s comliment or 2 2s comliment // ans -1
