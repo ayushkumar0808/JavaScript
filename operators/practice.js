a6c3504eb17a275eb74b64a1fddf14ba4bbd1aff
Ayush Kumar <kayush3647@gmail.com>
2026-09-05 20:59:25 +0530

    add

--- operators/practice.js
+++ operators/practice.js
@@ -1,12 +1,17 @@
-let a = parseInt(prompt("Enter a number"));
-let b = parseInt(prompt("Enter a number"));
-const add = a + b;
-const subtract = a - b;
-const multiplly = a * b;
-const devide = a / b;
-const modulo = a % b;
-console.log("Add:", add);
-console.log("subtract:", subtract);
-console.log("modulo:", modulo);
-console.log("multiplly:", multiplly);
-console.log("devide:", devide);
+// let a = parseInt(prompt("Enter a number"));
+// let b = parseInt(prompt("Enter a number"));
+// const add = a + b;
+// const subtract = a - b;
+// const multiplly = a * b;
+// const devide = a / b;
+// const modulo = a % b;
+// console.log("Add:", add);
+// console.log("subtract:", subtract);
+// console.log("modulo:", modulo);
+// console.log("multiplly:", multiplly);
+// console.log("devide:", devide);
+
+//2
+
+// let a = (b = c = 5);
+// console.log(a, b, c);
