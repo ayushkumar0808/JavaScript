340a556a309ea43f4ab33f02682c5038f1cad946
Ayush Kumar <kayush3647@gmail.com>
2026-09-07 19:10:23 +0530

    update

--- loops/practice.js
+++ loops/practice.js
@@ -1,8 +1,17 @@
 //1.  calculate the sum of n numbers
-let n = 100;
-let sum = 0;
+// let n = 100;
+// let sum = 0;
 
-for (let i = 0; i <= n; i++) {
-  sum += i;
-}
-console.log(sum);
+// for (let i = 0; i <= n; i++) {
+//   sum += i;
+// }
+// console.log(sum);
+
+//2. calculate the sum of m to n numbers.
+// let n = 100;
+// let sum = 0;
+// let m = 50;
+// for (let i = m; i <= n; i++) {
+//   sum += i;
+// }
+// console.log(sum);
