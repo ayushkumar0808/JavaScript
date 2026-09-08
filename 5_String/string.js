defa8640826aef30e254d7ea9b88ff22674f082e
Ayush Kumar <kayush3647@gmail.com>
2026-09-09 00:42:34 +0530

    add

--- 5_String/string.js
+++ 5_String/string.js
@@ -4,10 +4,14 @@
 // let str = "Ay\nush";
 // console.log(str);
 
-let str = "Ayush";
-console.log(str[1]);
-str[2] = "o";
-console.log(str); //immutable
+// let str = "Ayush";
+// console.log(str[1]);
+// str[2] = "o";
+// console.log(str); //immutable
 
-for (ch of str) console.log(ch);
-for (ch in str) console.log(ch);
+// for (ch of str) console.log(ch);
+// for (ch in str) console.log(ch);
+
+//properties
+let str = "Ayush";
+console.log(str.length);
