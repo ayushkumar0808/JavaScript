50afe6d01f5157e8d45fc323a82f19d9ae922604
Ayush Kumar <kayush3647@gmail.com>
2026-09-09 01:10:28 +0530

    add

---
+++ 5_String/string-methods.js
@@ -0,0 +1,17 @@
+// let str = "Ayush Kumar";
+// let upperCaseStr = str.toUpperCase();
+// let lowerCaseStr = str.toLowerCase();
+// let trimStr = str.trim();
+// console.log(str, upperCaseStr, lowerCaseStr, trimStr);
+
+let str = "he is boy and  boy";
+let check = str.includes("He");
+console.log(check);
+console.log(str.indexOf("b"));
+console.log(str.charAt(0));
+console.log(str.replace("boy", "girl"));
+console.log(str.replaceAll("boy", "girl"));
+console.log(str.slice(4, 9));
+
+// let str = "ayUsh";
+// console.log(`${str.charAt(0).toUpperCase()}${str.slice(1).toLowerCase()}`);
