e44e117f17598f887df38783829ab1015a4d438a
Ayush Kumar <kayush3647@gmail.com>
2026-09-09 00:20:13 +0530

    add

---
+++ 5_String/backtics.js
@@ -0,0 +1,12 @@
+let a = 5;
+let b = 7;
+let c = 8;
+let result = `Number 1=${a} Number 2=${b} Number 3=${c}`;
+console.log(result);
+
+let firstName = "Ayush";
+let secondName = "Kumar";
+let fullName = firstName + " " + secondName;
+console.log(fullName);
+let fullName2 = `${firstName} ${secondName}`;
+console.log(fullName2);
