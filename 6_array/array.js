4899d0b1e1e75c3be5e5c9e5c73e48e7f35381f4
Ayush Kumar <kayush3647@gmail.com>
2026-09-09 14:54:04 +0530

    add

---
+++ 6_array/array.js
@@ -0,0 +1,13 @@
+// let arr = [33, 45, 67, 87];
+// let arr2 = ["Ayush", "bob", "john"];
+// let arr3 = ["Ayush", 24, "blr"];
+// console.log(arr, arr2, arr3);
+
+//cuntractor Array
+
+// let arr = new Array(20, 30, 40);
+// console.log(arr);
+// let arr2 = [20];
+// let arr3 = new Array(20).fill(1);
+// console.log(arr2);
+// console.log(arr3);
