32f970cc7c1bf4ed2d681c908b62fad88fa4ab92
Ayush Kumar <kayush3647@gmail.com>
2026-09-09 18:08:21 +0530

    add

--- 6_array/array-methods.js
+++ 6_array/array-methods.js
@@ -1,7 +1,13 @@
-// let arr = [4, 8, 5, 9, 5];
+let arr = [4, 8, 5, 9, 5];
 // let a = arr.push(0);
 // console.log(arr, a);
 
 // let arr = [4, 8, 5, 9, 5];
 // let a = arr.pop();
 // console.log(arr, a);
+
+// let a = arr.unshift(0);
+// console.log(arr, a);
+
+let a = arr.shift();
+console.log(arr, a);
