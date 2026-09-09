93794c8b066858d556af3d2e5f0b83faf300a7e8
Ayush Kumar <kayush3647@gmail.com>
2026-09-09 18:10:44 +0530

    add

--- 6_array/array-methods.js
+++ 6_array/array-methods.js
@@ -9,5 +9,8 @@ let arr = [4, 8, 5, 9, 5];
 // let a = arr.unshift(0);
 // console.log(arr, a);
 
-let a = arr.shift();
-console.log(arr, a);
+// let a = arr.shift();
+// console.log(arr, a);
+
+// let arr1 = arr.slice(1, 3);
+// console.log(arr1);
