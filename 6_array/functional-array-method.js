8d3428977d959fbe5f75f04d86eafcc6aab6898c
Ayush Kumar <kayush3647@gmail.com>
2026-09-10 10:33:27 +0530

    add

--- 6_array/functional-array-method.js
+++ 6_array/functional-array-method.js
@@ -7,15 +7,20 @@ let arr = [4, 6, 8, 95, 0, 4, 6];
 // let arrNew = arr.map((elem, index, arr) => {
 //   return elem * 2;
 // });
-// console.log(arrNew);
+// console.log(arrNew);//new array
 
 // let sum = 0;
 // arr.forEach((elem) => {
 //   sum += elem;
 // });
-// console.log(sum);
+// console.log(sum);//undefiend
 
 // let arrNew = arr.filter((elem) => {
 //   return elem % 2 === 0;
 // });
-// console.log(arrNew);
+// console.log(arrNew);//new array
+
+// let val = arr.reduce((acc, curr) => {
+//   return acc + curr;
+// }, 10);
+// console.log(val);
