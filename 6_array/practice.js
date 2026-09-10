c627fff7751ee08d3eceb899d7c96c8b44fbcdf1
Ayush Kumar <kayush3647@gmail.com>
2026-09-10 10:41:07 +0530

    add

---
+++ 6_array/practice.js
@@ -0,0 +1,12 @@
+//for an array with marks  of students find the avarage marks of the entire class .
+
+// let marks = [90, 78, 65, 46, 89, 75];
+// let sum = 0;
+// let avg;
+// marks.forEach((elem) => {
+//   sum += elem;
+// });
+// avg = sum / marks.length;
+// console.log(avg);
+
+//
