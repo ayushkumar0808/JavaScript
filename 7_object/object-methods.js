38d18aa15d408e31188a38b54bbc1cfb5d369fba
Ayush Kumar <kayush3647@gmail.com>
2026-09-10 20:36:49 +0530

    add

--- 7_object/object-methods.js
+++ 7_object/object-methods.js
@@ -3,16 +3,23 @@ let obj = {
   age: 24,
 };
 
-let keys = Object.keys(obj);
-console.log(keys);
+// let keys = Object.keys(obj);
+// console.log(keys);
 
-let values = Object.values(obj);
-console.log(values);
+// let values = Object.values(obj);
+// console.log(values);
 
-let entities = Object.entries(obj);
-console.log(entities.flat());
+// let entities = Object.entries(obj);
+// console.log(entities.flat());
 
-for (let ch in obj) {
-  console.log(ch);
-  console.log(obj[ch]);
-}
+// for (let ch in obj) {
+//   console.log(ch);
+//   console.log(obj[ch]);
+// }
+
+let obj2 = {};
+Object.assign(obj2, obj);
+console.log(obj2);
+//spread operator
+let obj3 = { ...obj };
+console.log(obj3);
