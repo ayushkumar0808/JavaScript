116bceab3883410cfa12287a30fcfbcf314f547b
Ayush Kumar <kayush3647@gmail.com>
2026-09-11 19:15:38 +0530

    add

---
+++ 8_DOM_manuplation/basics.js
@@ -0,0 +1,10 @@
+/*
+five phase of DOM manuplation:
+
+1.DOM(Document Object Model)
+2.Selection of html elements 
+3.How to change or manupulate HTML
+4.How to change or manupulate CSS
+5.Event Litstener
+
+*/
