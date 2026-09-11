78b887e7c7d53c71c1313baf40dfa0c6187795c4
Ayush Kumar <kayush3647@gmail.com>
2026-09-11 19:27:41 +0530

    add

--- 8_DOM_manuplation/basics.js
+++ 8_DOM_manuplation/basics.js
@@ -8,3 +8,23 @@ five phase of DOM manuplation:
 5.Event Litstener
 
 */
+let h1 = document.querySelector("h1");
+let p = document.querySelector("p");
+
+// h1.innerHTML = "Yes He is Ayush Kumar";
+// p.innerHTML = "wow it's amazing";
+
+// h1.style.backgroundColor = "rgb(0,255,0)";
+// p.style.color = "purple";
+
+// h1.addEventListener(event, function () {});
+
+// h1.addEventListener("click", function () {
+//   h1.innerHTML = "Yes He is Ayush Kumar";
+//   h1.style.backgroundColor = "rgb(0,255,0)";
+// });
+
+// p.addEventListener("click", function () {
+//   p.innerHTML = "wow it's amazing";
+//   p.style.color = "purple";
+// });
