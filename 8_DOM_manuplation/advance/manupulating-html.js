48ffb6c364d21c5e84720b663fbdafae5d8376dc
Ayush Kumar <kayush3647@gmail.com>
2026-09-12 14:32:42 +0530

    add

---
+++ 8_DOM_manuplation/advance/manupulating-html.js
@@ -0,0 +1,12 @@
+//get tag name (read only)
+//.tagName
+//.nodeName
+
+// let a = document.querySelector("h1");
+// console.log(a.tagName);
+// console.log(a.nodeName);
+// console.log(a.firstChild.nodeName);
+
+// text get&se
+//innerText
+//textContent
