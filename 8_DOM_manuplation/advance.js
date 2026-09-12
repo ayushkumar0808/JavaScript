5d365d1f8c2bf911dd40e85a1e845b7b7761e91b
Ayush Kumar <kayush3647@gmail.com>
2026-09-12 13:47:35 +0530

    add

--- 8_DOM_manuplation/advance.js
+++ 8_DOM_manuplation/advance.js
@@ -4,3 +4,7 @@ let b = document.querySelectorAll("div");
 console.log(b);
 let a = document.getElementsByTagName("div");
 console.log(a);
+let c = document.getElementById("greet");
+console.log(c);
+let d = document.getElementsByClassName("hi");
+console.log(d);
