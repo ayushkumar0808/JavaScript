17d6500a0271aac16395060fbc6c51e2f7b1dce6
Ayush Kumar <kayush3647@gmail.com>
2026-09-13 14:05:51 +0530

    add

--- 8_DOM_manuplation/advance/manupulating-html.js
+++ 8_DOM_manuplation/advance/manupulating-html.js
@@ -1,13 +1,16 @@
-//get tag name (read only)
+let a = document.querySelector("div");
+
+//1.get tag name (read only)
+
 //.tagName
 //.nodeName
 
-let a = document.querySelector("div");
-// console.log(a.tagName);
+// // console.log(a.tagName);
 // console.log(a.nodeName);
 // console.log(a.firstChild.nodeName);
 
-// text get&se
+// 2.text get&se
+
 //innerText
 //textContent
 
@@ -16,15 +19,21 @@ let a = document.querySelector("div");
 // console.log(a.innerText);
 // console.log(a.textContent);
 
-//element/html get and set
+// 3.element/html get and set
+
 // a.innerHTML = "<p>baki kya hal</p> ";
 // console.log(a.innerHTML);
 
 // console.log(a);
 
+//#create  element:
+
 let elem = document.createElement("p");
 elem.innerText = "New para";
-a.append(elem);
+
+//#operations:
+
+// a.append(elem);
 // a.prepend(elem);
 // a.before(elem);
 // a.after(elem);
@@ -32,8 +41,18 @@ a.append(elem);
 // a.insertAdjacentElement("afterend", elem);
 // a.insertAdjacentElement("beforebegin", elem);
 // a.insertAdjacentElement("beforeend", elem);
-
+// a.replaceChild(elem, a.children[1]);
+// a.insertBefore(elem, a.children[2]);
 // a.remove();
 
-let p = a.children[2];
-p.remove();
+// let p = a.children[2];
+// p.remove();
+
+//acesses child to parent node
+
+// let p = document.querySelector("#a");
+// p.style.backgroundColor = "red";
+// let parentNode = p.parentNode;
+
+// console.log(parentNode);
+// parentNode.append(elem);
