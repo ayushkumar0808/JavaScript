7ecbbe528755716b8d1074281e9fe2fa88d99695
Ayush Kumar <kayush3647@gmail.com>
2026-09-16 10:16:52 +0530

    add

---
+++ 8_DOM_manuplation/advance/event-handling/event-handling.js
@@ -0,0 +1,9 @@
+let btn = document.querySelector(".btn");
+
+//property method
+btn.onclick = () => {
+  console.log("single click");
+};
+btn.ondblclick = () => {
+  console.log("double click");
+};
