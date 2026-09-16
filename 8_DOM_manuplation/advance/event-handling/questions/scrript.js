db3e961698a83d08f4c75d94053d45aed4fbc86c
Ayush Kumar <kayush3647@gmail.com>
2026-09-16 18:56:18 +0530

    add

--- 8_DOM_manuplation/advance/event-handling/questions/scrript.js
+++ 8_DOM_manuplation/advance/event-handling/questions/scrript.js
@@ -1,15 +1,17 @@
-let btnDec = document.querySelector("#btnDec");
-let btnInc = document.querySelector("#btnInc");
-let currentValue = document.querySelector("#currentValue");
-let val = 0;
-btnDec.addEventListener("click", () => {
-  if (val <= 0) {
-    return;
-  }
-  val = val - 1;
-  currentValue.innerText = val;
-});
-btnInc.addEventListener("click", () => {
-  val = val + 1;
-  currentValue.innerText = val;
-});
+// let btnDec = document.querySelector("#btnDec");
+// let btnInc = document.querySelector("#btnInc");
+// let currentValue = document.querySelector("#currentValue");
+// let val = 0;
+// btnDec.addEventListener("click", () => {
+//   if (val <= 0) {
+//     return;
+//   }
+//   val = val - 1;
+//   currentValue.innerText = val;
+// });
+// btnInc.addEventListener("click", () => {
+//   val = val + 1;
+//   currentValue.innerText = val;
+// });
+
+///question 2
