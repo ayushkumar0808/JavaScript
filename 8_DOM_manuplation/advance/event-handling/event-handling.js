abb8944b6c97f6f1dd645fe1e60b9cba4cfd1dc0
Ayush Kumar <kayush3647@gmail.com>
2026-09-16 10:32:14 +0530

    add

--- 8_DOM_manuplation/advance/event-handling/event-handling.js
+++ 8_DOM_manuplation/advance/event-handling/event-handling.js
@@ -1,9 +1,33 @@
 let btn = document.querySelector(".btn");
 
 //property method
-btn.onclick = () => {
-  console.log("single click");
-};
-btn.ondblclick = () => {
-  console.log("double click");
-};
+// btn.onclick = () => {
+//   console.log("single click");
+// };
+// btn.ondblclick = () => {
+//   console.log("double click");
+// };
+// btn.onclick = () => {
+//   console.log("property wala click");
+// };
+
+//listener method
+// node.addEventListener("event", function () {}); syntax
+// btn.addEventListener("click", () => {
+//   console.log("single click");
+// });
+// btn.addEventListener("click", () => {
+//   console.log("dusra single click");
+// });
+
+// // remove event
+// function fn() {
+//   console.log("helo");
+// }
+// btn.addEventListener("click", fn);
+// btn.removeEventListener("click", fn);
+
+// event Object
+// btn.addEventListener("click", function (event) {
+//   console.log(event);
+// });
