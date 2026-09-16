9105d012bf5ee171016655c2d2289baa0e94ba82
Ayush Kumar <kayush3647@gmail.com>
2026-09-16 19:15:16 +0530

    add

--- 8_DOM_manuplation/advance/event-handling/questions/scrript.js
+++ 8_DOM_manuplation/advance/event-handling/questions/scrript.js
@@ -15,3 +15,23 @@
 // });
 
 ///question 2
+
+let form = document.querySelector("form");
+let inputName = document.querySelector("#name");
+let inputEmail = document.querySelector("#email");
+let submitBtn = document.querySelector("#submit");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (!inputName.value || !inputEmail.value) {
+    console.error("Fill the both field");
+    alert("Fill the both field");
+    return;
+  }
+  let div = document.createElement("div");
+  div.innerHTML = `<p>Name: ${inputName.value}</p>
+<span>Email: ${inputEmail.value}</span>
+`;
+  document.querySelector("body").append(div);
+
+  console.log(inputName.value, inputEmail.value);
+});
