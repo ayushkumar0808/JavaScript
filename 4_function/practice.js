0093b197ec74d8f93dbc4bc684149bbc748a4618
Ayush Kumar <kayush3647@gmail.com>
2026-09-20 14:43:55 +0530

    update

--- 4_function/practice.js
+++ 4_function/practice.js
@@ -14,3 +14,18 @@
 //   console.log(str.toLowerCase() + "_" + str.length);
 // };
 // username("Ayush");
+
+// const traffic = (color) => {
+//   if (color.toLowerCase() === "green") {
+//     console.log("Go");
+//   } else if (color.toLowerCase() === "yellow") {
+//     console.log("Caution");
+//   } else if (color.toLowerCase() === "red") {
+//     console.log("Stop");
+//   } else {
+//     console.log("invaild color");
+//   }
+// };
+// traffic("REd");
+// traffic("green");
+// traffic("Yello");
