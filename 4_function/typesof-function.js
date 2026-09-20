25d26c9bd37e19475349e301d1b753cbbb364a1a
Ayush Kumar <kayush3647@gmail.com>
2026-09-20 14:17:33 +0530

    update

---
+++ 4_function/typesof-function.js
@@ -0,0 +1,31 @@
+// function declration
+// function add(a, b) {
+//   return a + b;
+// }
+// let sum = add(5, 6);
+// console.log(sum);
+
+//function expression
+// let add = function (a, b) {
+//   return a + b;
+// };
+// let sum = add(5, 6);
+// console.log(sum);
+
+//Arrow funtion
+// let add = (a, b) => {
+//   return a + b;
+// };
+// let sum = add(5, 6);
+// console.log(sum);
+
+//Anonymous function
+// setTimeout(() => {
+//   console.log(9 + 8);
+// }, 3000);
+
+// imidiately invoke function expression
+
+(() => {
+  console.log("hi");
+})();
