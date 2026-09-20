4654ba19e34031a03ae64c672bec258aae6f24a0
Ayush Kumar <kayush3647@gmail.com>
2026-09-20 23:18:55 +0530

    update

--- 9_event_lifecycle/event_propogation/event_propogation.js
+++ 9_event_lifecycle/event_propogation/event_propogation.js
@@ -2,35 +2,68 @@ let grandParent = document.querySelector(".grand_parent");
 let parent = document.querySelector(".parent");
 let child = document.querySelector(".child");
 //capturing target bubbling by default bubbling
-grandParent.addEventListener(
-  "click",
-  () => {
-    alert("grand parent call");
-  },
-  true,
-);
-grandParent.addEventListener("click", () => {
-  alert("grand parent call");
+// grandParent.addEventListener(
+//   "click",
+//   () => {
+//     alert("grand parent call");
+//   },
+//   true,
+// );
+
+// parent.addEventListener(
+//   "click",
+//   () => {
+//     alert("parent call");
+//   },
+//   true,
+// );
+
+// child.addEventListener(
+//   "click",
+//   () => {
+//     alert(" child call");
+//   },
+//   true,
+// );
+
+// grandParent.addEventListener("click", (e) => {
+//   console.log(e.target);
+//   console.log(e.currentTarget);
+//   console.log(this);
+// });
+
+// parent.addEventListener("click", (e) => {
+//   console.log(e.target);
+//   console.log(e.currentTarget);
+//   console.log(this);
+// });
+
+// child.addEventListener("click", (e) => {
+//   console.log(e.target);
+//   console.log(e.currentTarget);
+//   console.log(this);
+// });
+
+//stop propagation
+
+grandParent.addEventListener("click", (e) => {
+  console.log(e.target);
+  console.log(e.currentTarget);
 });
 
-parent.addEventListener(
-  "click",
-  () => {
-    alert("parent call");
-  },
-  true,
-);
-parent.addEventListener("click", () => {
-  alert("parent call");
+parent.addEventListener("click", (e) => {
+  console.log(e.target);
+  console.log(e.currentTarget);
+  //   e.stopPropagation();
+  e.stopImmediatePropagation();
 });
 
-child.addEventListener(
-  "click",
-  () => {
-    alert(" child call");
-  },
-  true,
-);
-child.addEventListener("click", () => {
-  alert(" child call");
+parent.addEventListener("click", (e) => {
+  console.log("parent next");
+});
+
+child.addEventListener("click", (e) => {
+  console.log(e.target);
+  console.log(e.currentTarget);
+  //   e.stopPropagation();
 });
