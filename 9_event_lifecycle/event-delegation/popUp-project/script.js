b5ca1279c834bbbd4d08f5786caa34a9fa66664b
Ayush Kumar <kayush3647@gmail.com>
2026-09-21 11:56:28 +0530

    add

---
+++ 9_event_lifecycle/event-delegation/popUp-project/script.js
@@ -0,0 +1,11 @@
+let btn = document.querySelector(".btn");
+let container = document.querySelector(".container");
+// let close = document.querySelector(".close");
+
+btn.addEventListener("click", () => {
+  container.style.display = "flex";
+});
+
+// close.addEventListener("click", () => {
+//   container.style.display = "none";
+// });
