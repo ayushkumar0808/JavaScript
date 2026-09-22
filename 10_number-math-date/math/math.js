e392b8e81b7cc18146f21d60f50dba1c2b6926a3
Ayush Kumar <kayush3647@gmail.com>
2026-09-23 00:35:27 +0530

    add

--- 10_number-math-date/math/math.js
+++ 10_number-math-date/math/math.js
@@ -4,3 +4,6 @@
 // console.log(Math.ceil(n));
 // console.log(Math.abs(-9));
 // console.log(Math.sqrt(9));
+// console.log(Math.pow(3, 2));
+// console.log(Math.min(45, 3445, 54, 35, 25));
+// console.log(Math.max(45, 3445, 54, 35, 25));
