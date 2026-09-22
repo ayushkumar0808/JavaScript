2bad0f974435101bfba39a34f78499c677537866
Ayush Kumar <kayush3647@gmail.com>
2026-09-22 23:34:24 +0530

    add

--- 10_number-math-date/nubmer/number.js
+++ 10_number-math-date/nubmer/number.js
@@ -10,5 +10,9 @@
 
 //number properties:
 
-console.log(Number.MAX_VALUE);
-console.log(Number.MIN_VALUE);
+// console.log(Number.MAX_VALUE);
+// console.log(Number.MIN_VALUE);
+// console.log(Number.POSITIVE_INFINITY);
+// console.log(Number.NEGATIVE_INFINITY);
+// console.log(Number.NaN);
+// console.log(Number.EPSILON);
