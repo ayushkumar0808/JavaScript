498a6e710309e73b13ba40ed4ba5144889a41cc7
Ayush Kumar <kayush3647@gmail.com>
2026-09-24 17:26:42 +0530

    add

--- 10_number-math-date/math/math.js
+++ 10_number-math-date/math/math.js
@@ -6,4 +6,8 @@
 // console.log(Math.sqrt(9));
 // console.log(Math.pow(3, 2));
 // console.log(Math.min(45, 3445, 54, 35, 25));
-// console.log(Math.max(45, 3445, 54, 35, 25));
+// console.log(Math.random());
+
+//write a function that gives random no between 2 no
+
+console.log(Math.floor(Math.random() * 20 + 1) + 5);
