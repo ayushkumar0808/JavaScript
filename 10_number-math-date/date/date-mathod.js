08bba54b9395bd143afba507ccfdf4e6bb21d294
Ayush Kumar <kayush3647@gmail.com>
2026-09-25 18:35:38 +0530

    add

--- 10_number-math-date/date/date-mathod.js
+++ 10_number-math-date/date/date-mathod.js
@@ -13,5 +13,6 @@ console.log(date.getDate());
 console.log(date.getHours());
 console.log(date.getMinutes());
 console.log(date.getSeconds());
+console.log(date.getMilliseconds());
 
 console.log(date.getTimezoneOffset() / 60);
