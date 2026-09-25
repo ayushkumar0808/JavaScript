ca1d1469f99d2770281001ecbc883f8fcf5f36a7
Ayush Kumar <kayush3647@gmail.com>
2026-09-25 18:34:28 +0530

    add

--- 10_number-math-date/date/date-mathod.js
+++ 10_number-math-date/date/date-mathod.js
@@ -7,8 +7,11 @@ let date = new Date();
 // console.log(date.toLocaleString());
 
 console.log(date.getFullYear());
-console.log(date.getMonth());
+console.log(date.getMonth()); //zero index
 console.log(date.getDate());
 // console.log(date.getDay());
 console.log(date.getHours());
 console.log(date.getMinutes());
+console.log(date.getSeconds());
+
+console.log(date.getTimezoneOffset() / 60);
