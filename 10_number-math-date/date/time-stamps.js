68cfa64d206a3d10ff74f2b7a59c23e34e03d3e9
Ayush Kumar <kayush3647@gmail.com>
2026-09-25 23:48:20 +0530

    add

--- 10_number-math-date/date/time-stamps.js
+++ 10_number-math-date/date/time-stamps.js
@@ -3,3 +3,14 @@
 // console.log(date, date1);
 // let date2 = new Date(1790356936704);
 // console.log(date2);
+
+let currentTime = Date.now();
+console.log(currentTime);
+
+let tenMin = 1000 * 60 * 10;
+
+let updatedTime = currentTime + tenMin;
+console.log(updatedTime);
+
+let date = new Date(updatedTime);
+console.log(date.toLocaleString());
