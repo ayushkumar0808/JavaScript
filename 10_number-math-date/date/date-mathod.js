05cd6c02e0beee932299afdf0ac0f3595e38b8a6
Ayush Kumar <kayush3647@gmail.com>
2026-09-25 18:31:22 +0530

    add

---
+++ 10_number-math-date/date/date-mathod.js
@@ -0,0 +1,14 @@
+// let date = Date.now();//unix time
+// //start from 1 jan 1970 in cs
+// console.log(date);
+
+let date = new Date();
+
+// console.log(date.toLocaleString());
+
+console.log(date.getFullYear());
+console.log(date.getMonth());
+console.log(date.getDate());
+// console.log(date.getDay());
+console.log(date.getHours());
+console.log(date.getMinutes());
